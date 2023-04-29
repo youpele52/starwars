e37c8f511d0f235fd5e0dba6107d0fcd19248d2e
Youpele Michael <mjayjesus@gmail.com>
2023-04-29 02:59:24 +0200

    Update planet.tsx

--- src/pages/planet.tsx
+++ src/pages/planet.tsx
@@ -25,7 +25,7 @@ export default function Planet() {
     return (
       <>
         <Head>
-          <title>Star Wars | Planet |{planet.name}</title>
+          <title>Star Wars | Planet | {planet.name}</title>
           <meta name="description" content="Star Wars" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
